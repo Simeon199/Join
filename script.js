181f38e66f3bb55e92d07abc866b2727044650cc
Marcel Zalec <marci.zalec@hotmail.com>
2024-05-30 10:33:28 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,35 @@
+const BASE_URL = 'https://join-privat-default-rtdb.europe-west1.firebasedatabase.app/';
+
+function goToSignUp() {
+    window.location.href = 'signup.html'
+}
+
+function validatePassword() {
+    let msgbox = document.getElementById('msgbox');
+    //if (password !== passwordConfirm) {
+    //    msgbox.innerHTML = 'Password incorrect';
+    //    return false;
+    //}
+    //return true;
+    msgbox.innerHTML = "Password incorrect";
+}
+
+function validateCheckbox() {
+    let checkbox = document.getElementById('remember');
+    let loginBTN = document.getElementById('loginBtn');
+    if (checkbox.checked = true) {
+        loginBTN.enabled = true;
+    } else {
+        loginBTN.enabled = false;
+    }
+}
+
+function login() {
+    //validateCheckbox();
+    // validatePassword();
+    window.location.href='board.html'
+}
+
+function goToSignUp() {
+    window.location.href="register.html"
+}
