1b4289394755afcf92c374525593d412804f5083
Marcel Zalec <marci.zalec@hotmail.com>
2024-06-07 12:45:41 +0200

    sow_contacts hinzugefügt

---
+++ js/sow_contacts.js
@@ -0,0 +1,7 @@
+const BASE_URL = 'https://join-privat-default-rtdb.europe-west1.firebasedatabase.app/';
+
+async function loadData(path="") {
+    let response = await fetch(BASE_URL + path + '.json');
+    let responseAsJson = await response.json();
+    console.log(responseAsJson)
+}
