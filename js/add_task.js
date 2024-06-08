28fb781cd1f78ce563e696f0c74d5fd14ccefda1
Marcel Zalec <marci.zalec@hotmail.com>
2024-06-08 14:24:55 +0200

    update add_Task

--- js/add_task.js
+++ js/add_task.js
@@ -1,3 +1,27 @@
 function taskMarker() {
     document.getElementById('addTask').classList.add('currentSection');
 }
+
+function changePriority(id) {
+    removeBackground(id)
+    if (id == urgent) {
+        urgent.classList.add("backgroundUrgent")
+    } if (id == medium) {
+        medium.classList.add("backgroundMedium")
+    } if (id == low) {
+        low.classList.add("backgroundLow")
+    }
+}
+
+function removeBackground(id) {
+    if (id == urgent) {
+        medium.classList.remove("backgroundMedium")
+        low.classList.remove("backgroundLow")
+    } if (id == medium) {
+        urgent.classList.remove("backgroundUrgent")
+        low.classList.remove("backgroundLow")
+    } if (id == low) {
+        urgent.classList.remove("backgroundUrgent")
+        medium.classList.remove("backgroundMedium")
+    }
+}
