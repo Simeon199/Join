def24be43f8652ee98b5b0bb1e79edf03ccc54a7
Mr.X <aq-computer@outlook.com>
2024-06-09 22:38:08 +0800

    Added contact functions (render, add, delete) and updated CSS styles

--- js/sow_contacts.js
+++ js/sow_contacts.js
@@ -1,7 +1,7 @@
-const BASE_URL = 'https://join-privat-default-rtdb.europe-west1.firebasedatabase.app/';
+// const BASE_URL = 'https://join-privat-default-rtdb.europe-west1.firebasedatabase.app/';
 
-async function loadData(path="") {
-    let response = await fetch(BASE_URL + path + '.json');
-    let responseAsJson = await response.json();
-    console.log(responseAsJson)
+async function loadData(path = "") {
+  let response = await fetch(BASE_URL + path + ".json");
+  let responseAsJson = await response.json();
+  console.log(responseAsJson);
 }
