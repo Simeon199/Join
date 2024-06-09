b022775d9b0cfe05f3a4aebdf397e5e066a4307c
Simeon199 <sim.ki@outlook.de>
2024-06-09 18:23:07 +0200

    revised login-function

--- js/sow_contacts.js
+++ js/sow_contacts.js
@@ -1,4 +1,4 @@
-// const BASE_URL = 'https://join-privat-default-rtdb.europe-west1.firebasedatabase.app/';
+const BASE_URL = 'https://join-privat-default-rtdb.europe-west1.firebasedatabase.app/';
 
 async function loadData(path = "") {
   let response = await fetch(BASE_URL + path + ".json");
--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 // const BASE_URL = 'https://join-privat-default-rtdb.europe-west1.firebasedatabase.app/';
 
+let isLoggedIn = false;
+
 function validatePassword() {
     let msgbox = document.getElementById('msgbox');
     //if (password !== passwordConfirm) {
@@ -20,69 +22,6 @@ function validateCheckbox() {
     }
 }
 
-// Hier kommen meine ganzen neuen Funktionen //
-
-function setCookie(name, value, days) {
-    let expires = "";
-    if (days) {
-        let date = new Date();
-        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
-        expires = "; expires=" + date.toUTCString();
-    }
-    document.cookie = name + "=" + value + expires + "; path=/";
-}
-
-function getCookie(name) {
-    let nameEQ = name + "=";
-    let cookies = document.cookie.split(';');
-    for (let i = 0; i < cookies.length; i++) {
-        let cookie = cookies[i];
-        while (cookie.charAt(0) == ' ') {
-            cookie = cookie.substring(1, cookie.length);
-        }
-        if (cookie.indexOf(nameEQ) == 0) {
-            return cookie.substring(nameEQ.length, cookie.length);
-        }
-    }
-    return null;
-}
-
-function deleteCookie(name) {
-    document.cookie = name + "=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-}
-
-// Hier enden meine ganzen neuen Funktionen //
-
-async function testLoginFunction(){
-    let loginEmail = document.getElementById('loginEmail').value;
-    let loginPassword = document.getElementById('loginPassword').value;
-    let remember = document.getElementById('remember').checked;
-    let response = await loadData(path="");
-    // console.log(response);
-    for(key in response){
-        let user = response[key];
-        if(user["email"] && user["password"]){
-            let email = user["email"];
-            let password = user["password"];
-            if(loginEmail == email && loginPassword == password){
-                if(remember){
-                    // Set cookie for 30 days if "remember me" is checked 
-                    setCookie('authToken', 'YourAuthTokenValue', 30);
-                } else {
-                    // Set the cookie for the session (no expiration date)
-                    setCookie('authToken', 'YourAuthTokenValue', 0);
-                }
-                window.location.href = "summary.html";
-                return;
-            } else {
-                alert("Eingegebene E-Mail oder Passwort sind falsch! Bitte versuchen Sie es erneut");
-            }
-        } else {
-            alert('E-Mail oder Passwort wurden nicht übergeben!');
-        }
-    }
-}
-
 function login() {
     validateCheckbox();
     validatePassword();
@@ -97,7 +36,59 @@ function backToLogin() {
     window.location.href= "login.html"
 }
 
-function signUp(){
+function invokeFunctions(){
+    loadData();
+    checkIfUserIsLoggedIn();
+}
+
+function logout(){
+    isLoggedIn = false;
+    localStorage.item = isLoggedIn;
+}
+
+function checkIfUserIsLoggedIn(){
+    let status = localStorage.getItem('isLoggedIn');
+    if(status == 'true'){
+        window.location.href = 'summary.html';
+    }
+}
+
+function saveLoggedInStatus(){
+    localStorage.setItem('isLoggedIn', isLoggedIn);
+}
+
+async function testLoginFunction(){
+    let loginEmail = document.getElementById('loginEmail').value;
+    let loginPassword = document.getElementById('loginPassword').value;
+    let remember = document.getElementById('remember').checked;
+    let response = await loadData(path="");
+    for(let key in response){
+        let user = response[key];
+        if(user["email"] && user["password"]){
+            let email = user["email"];
+            let password = user["password"];
+            if(loginEmail == email && loginPassword == password){
+                if(remember){
+                    isLoggedIn = true;
+                    saveLoggedInStatus();
+                } else {
+                    isLoggedIn = false;
+                    saveLoggedInStatus();
+                }
+                window.location.href = "summary.html";
+                return;
+            } else {
+                alert("Eingegebene E-Mail oder Passwort sind falsch! Bitte versuchen Sie es erneut");
+                return;
+            }
+        } else {
+            alert('E-Mail oder Passwort wurden nicht übergeben!');
+        }
+    }
+}
+
+function signUp(event){
+    event.preventDefault();
     let name = document.getElementById('name').value;
     let email = document.getElementById('loginEmail').value;
     let password = document.getElementById('loginPassword').value;
@@ -155,7 +146,6 @@ async function pushNewUserToDataBase(path="", user){
         responseToJson = await response.json();
         let registerPopup = document.getElementById('registerPopup');
         registerPopup.classList.remove('d-none');
-        // alert("Registrierung erfolgreich!");
         setTimeout(() => {
             window.location.href = 'login.html';
         }, 2000); 
