d8b282744e179d4596755d17270c675ff0d979f4
Mr.X <aq-computer@outlook.com>
2024-06-15 22:44:22 +0800

    Made header and sidebar responsive

--- js/add_task.js
+++ js/add_task.js
@@ -4,76 +4,82 @@ let assignetTo = document.getElementById("assignetTo");
 let date = document.getElementById("date");
 let category = document.getElementById("category");
 let subtask = document.getElementById("subtask");
- 
+
 function init() {
-    changePriority(medium)
+  changePriority(medium);
 }
 
 function taskMarker() {
-    document.getElementById('addTask').classList.add('currentSection');
+  document.getElementById("addTask").classList.add("currentSection");
 }
 
 function changePriority(id) {
-    removeBackground(id)
-    if (id == urgent) {
-        urgent.classList.add("backgroundUrgent")
-    } if (id == medium) {
-        medium.classList.add("backgroundMedium")
-    } if (id == low) {
-        low.classList.add("backgroundLow")
-    }
-    changeImg(id)
+  removeBackground(id);
+  if (id == urgent) {
+    urgent.classList.add("backgroundUrgent");
+  }
+  if (id == medium) {
+    medium.classList.add("backgroundMedium");
+  }
+  if (id == low) {
+    low.classList.add("backgroundLow");
+  }
+  changeImg(id);
 }
 
 function removeBackground(id) {
-    if (id == urgent) {
-        medium.classList.remove("backgroundMedium")
-        low.classList.remove("backgroundLow")
-    } if (id == medium) {
-        urgent.classList.remove("backgroundUrgent")
-        low.classList.remove("backgroundLow")
-    } if (id == low) {
-        urgent.classList.remove("backgroundUrgent")
-        medium.classList.remove("backgroundMedium")
-    }
+  if (id == urgent) {
+    medium.classList.remove("backgroundMedium");
+    low.classList.remove("backgroundLow");
+  }
+  if (id == medium) {
+    urgent.classList.remove("backgroundUrgent");
+    low.classList.remove("backgroundLow");
+  }
+  if (id == low) {
+    urgent.classList.remove("backgroundUrgent");
+    medium.classList.remove("backgroundMedium");
+  }
 }
 
 function changeImg(condition) {
-    let urgentImg = document.getElementById('imgUrgent');
-    let mediumImg = document.getElementById('imgMedium');
-    let lowImg = document.getElementById('imgLow');
-    if (condition == urgent) {
-        urgentImg.setAttribute('src', 'Assets/img/Prio altaurgent_white.svg')
-    } else {
-        urgentImg.setAttribute('src', 'Assets/img/Prio altaUrgent_symbole.svg')
-    } if (condition == medium) {
-        mediumImg.setAttribute('src', 'Assets/img/Prio mediameduim_white.svg')
-    } else {
-        mediumImg.setAttribute('src', 'Assets/img/Capa 1medium_color.svg')
-    } if (condition == low) {
-        lowImg.setAttribute('src', 'Assets/img/Prio bajalow_white.svg')
-    } else {
-        lowImg.setAttribute('src', 'Assets/img/Prio bajaLow_symbole.svg')
-    }
+  let urgentImg = document.getElementById("imgUrgent");
+  let mediumImg = document.getElementById("imgMedium");
+  let lowImg = document.getElementById("imgLow");
+  if (condition == urgent) {
+    urgentImg.setAttribute("src", "Assets/img/Prio altaurgent_white.svg");
+  } else {
+    urgentImg.setAttribute("src", "Assets/img/Prio altaUrgent_symbole.svg");
+  }
+  if (condition == medium) {
+    mediumImg.setAttribute("src", "Assets/img/Prio mediameduim_white.svg");
+  } else {
+    mediumImg.setAttribute("src", "Assets/img/Capa 1medium_color.svg");
+  }
+  if (condition == low) {
+    lowImg.setAttribute("src", "Assets/img/Prio bajalow_white.svg");
+  } else {
+    lowImg.setAttribute("src", "Assets/img/Prio bajaLow_symbole.svg");
+  }
 }
 
 function createTask() {
-    console.log("create");
+  console.log("create");
 }
 
 function clearTask() {
-    console.log(inputTitle.value);
-    inputTitle.value= '';
-    // inputDescription.value='';
-    // assignetTo.value='';
-    // date.value = '';
-    // category.value = '';
-    // subtask.value='';
+  console.log(inputTitle.value);
+  inputTitle.value = "";
+  // inputDescription.value='';
+  // assignetTo.value='';
+  // date.value = '';
+  // category.value = '';
+  // subtask.value='';
 }
 
 function showDropDownAssignedTo() {
-    document.getElementById('assignedToDropDown').classList.remove('d-none');
-    document.getElementById('assignedToDropDown').innerHTML = /*html*/`
+  document.getElementById("assignedToDropDown").classList.remove("d-none");
+  document.getElementById("assignedToDropDown").innerHTML = /*html*/ `
             <div onclick="hideDropDownAssignedTo()"><span>Test1</span></div>
             <div><span>Test2</span></div>
             <div><span>Test3</span></div>
@@ -81,21 +87,21 @@ function showDropDownAssignedTo() {
 }
 
 function showDropDownCategory() {
-    document.getElementById('categoryDropDown').classList.remove('d-none');
-    document.getElementById('categoryDropDown').innerHTML = /*html*/`
-            <div onclick="hideDropDownCategory(); changeCategory(Technical Task)"><span>Technical Task</span></div>
-            <div onclick="hideDropDownCategory(); changeCategory(Test)"><span>User Story</span></div>
+  document.getElementById("categoryDropDown").classList.remove("d-none");
+  document.getElementById("categoryDropDown").innerHTML = /*html*/ `
+            <div onclick="hideDropDownCategory(); changeCategory('Technical Task')"><span>Technical Task</span></div>
+            <div onclick="hideDropDownCategory(); changeCategory('Test')"><span>User Story</span></div>
     `;
 }
 
 function hideDropDownAssignedTo() {
-    document.getElementById('assignedToDropDown').classList.add('d-none');
+  document.getElementById("assignedToDropDown").classList.add("d-none");
 }
 
 function hideDropDownCategory() {
-    document.getElementById('categoryDropDown').classList.add('d-none');
+  document.getElementById("categoryDropDown").classList.add("d-none");
 }
 
 function changeCategory(text) {
-    document.getElementById('categoryText').innerHTML = `${text}`
+  document.getElementById("categoryText").innerHTML = `${text}`;
 }
