ee8d4a8ebc5377d9fb2193e8641d514a9163cba7
Simeon199 <sim.ki@outlook.de>
2024-06-24 16:44:09 +0200

    Some changes to styling and drag and drop (still not working perfectly)

--- js/board.js
+++ js/board.js
@@ -1,31 +1,43 @@
-let realTasks = [
+let testingTasks1 = [
   {
     "category": "to-do-container",
     "story-category": "User Story",
     "id": 0,
-    "title": "Kochwelt Page & Recipe Recommender",
-    "task": "Build start page with recipe recommendation"
+    "title": "Contact Form and Imprint",
+    "task": "Create a contact form and imprint page",
+    "people-in-charge": ['AS', 'DE', 'EF']
   },
   {
     "category": "await-feedback-container",
     "story-category": "Technical Task",
     "id": 1,
     "title": "HTML Base Template Creation",
-    "task": "Create reusable HTML base templates"
+    "task": "Create reusable HTML base templates",
+    "people-in-charge": ['AM', 'EM', 'MB']
   },
   {
     "category": "await-feedback-container",
     "story-category": "User Story",
     "id": 2,
     "title": "Daily Kochwelt Recipe",
-    "task": "Implement daily recipe and portion calculator"
+    "task": "Implement daily recipe and portion calculator",
+    "people-in-charge": ['EF', 'AS', 'TW']
   },
   {
     "category": "done-container",
     "story-category": "Technical Task",
     "id": 3,
     "title": "CSS Architecture Planning",
-    "task": "Define CSS naming conventions and structure"
+    "task": "Define CSS naming conventions and structure",
+    "people-in-charge": ['SM', 'BZ', 'TW']
+  },
+  {
+    "category": "in-progress-container",
+    "story-category": "User Story",
+    "id": 4,
+    "title": "Kochwelt Page & Recipe Recommender",
+    "task": "Build start page with recipe recommendation",
+    "people-in-charge": ['AM', 'EM', 'MB']
   }
 ]
 
@@ -96,7 +108,7 @@ async function postData(path = "", data = tasksObject) {
   }
 }
 
-// postData("tasks", realTasks).then(response => {
+// postData("tasks", testingTasks1).then(response => {
 //   console.log('Response from Firebase:', response);
 // });
 
@@ -104,7 +116,7 @@ async function loadTasksFromDatabase(){
   let response = await loadData();
   for(key in response){
     if(key == "tasks"){
-      let result = response[key]["-O05H016uL_VT-vaNnYE"];
+      let result = response[key]['-O09aaux8kE0fRXZS5KE'];
       console.log(result);
       return result;
     }
@@ -136,18 +148,70 @@ function updateHTML() {
   }
 }
 
-function createToDoHTML(element){
-  let variableClass;
+function setVariableClass(element){
+  let variableClass = '';
   if(element['story-category'] == 'User Story'){
     variableClass = 'task-category';
   } else if(element['story-category'] == 'Technical Task'){
     variableClass = 'technical-task-category';
   }
+  return variableClass;
+}
+
+function createToDoHTML(element){
+  let variableClass = setVariableClass(element);
+  console.log(variableClass);
+  // console.log(element["people-in-charge"]);
+  // let variableClass;
+  // if(element['story-category'] == 'User Story'){
+  //   variableClass = 'task-category';
+  // } else if(element['story-category'] == 'Technical Task'){
+  //   variableClass = 'technical-task-category';
+  // }
+  // return `<div class="task" draggable="true" ondragstart="startDragging(${element['id']})">
+  //           <button class=${variableClass}>${element['story-category']}</button>
+  //           <h3>${element['title']}</h2>
+  //           <p>${element['task']}</p>
+  //         </div>`;
   return `<div class="task" draggable="true" ondragstart="startDragging(${element['id']})">
-            <button class=${variableClass}>${element['story-category']}</button>
-            <h3>${element['title']}</h2>
-            <p>${element['task']}</p>
-          </div>`;
+            <div class='${variableClass}'>${element['story-category']}</div>
+
+            <h3 class="task-title">${element['title']}</h3>
+            <p class="task-description">${element['task']}</p>
+
+            <div class="task-bar-container">
+              <div class="task-bar">
+                <div class="task-bar-content"></div>
+              </div>
+
+              <p class="task-bar-text">1/2 Subtasks</p>
+            </div>
+
+            <div class="task-contacts-container">
+              <div class="task-contacts">
+                <div class="task-contact">${element['people-in-charge'][0]}</div>
+                <div class="task-contact">${element['people-in-charge'][1]}</div>
+                <div class="task-contact">${element['people-in-charge'][2]}</div>
+              </div>
+
+              <svg
+                width="18"
+                height="8"
+                viewBox="0 0 18 8"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M16.5685 7.16658L1.43151 7.16658C1.18446 7.16658 0.947523 7.06773 0.772832 6.89177C0.598141 6.71581 0.5 6.47716 0.5 6.22831C0.5 5.97947 0.598141 5.74081 0.772832 5.56485C0.947523 5.38889 1.18446 5.29004 1.43151 5.29004L16.5685 5.29004C16.8155 5.29004 17.0525 5.38889 17.2272 5.56485C17.4019 5.74081 17.5 5.97947 17.5 6.22831C17.5 6.47716 17.4019 6.71581 17.2272 6.89177C17.0525 7.06773 16.8155 7.16658 16.5685 7.16658Z"
+                  fill="#FFA800"
+                />
+                <path
+                  d="M16.5685 2.7098L1.43151 2.7098C1.18446 2.7098 0.947523 2.61094 0.772832 2.43498C0.598141 2.25902 0.5 2.02037 0.5 1.77152C0.5 1.52268 0.598141 1.28403 0.772832 1.10807C0.947523 0.932105 1.18446 0.833252 1.43151 0.833252L16.5685 0.833252C16.8155 0.833252 17.0525 0.932105 17.2272 1.10807C17.4019 1.28403 17.5 1.52268 17.5 1.77152C17.5 2.02037 17.4019 2.25902 17.2272 2.43498C17.0525 2.61094 16.8155 2.7098 16.5685 2.7098Z"
+                  fill="#FFA800"
+                />
+              </svg>
+            </div>
+          </div>`
 }
 
 function startDragging(element){
