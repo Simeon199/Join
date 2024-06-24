4de4425e6d65ebe24042052a9946544662b018f5
Mr.X <aq-computer@outlook.com>
2024-06-24 09:30:26 +0800

    Responsive summary page

--- js/summary.js
+++ js/summary.js
@@ -1,44 +1,26 @@
 let userName = getUserNickname();
 
 function init() {
-    greet();
+  greet();
 }
 
 function greet() {
-    cont =document.getElementById('greetingCont');
-    if (userName = "Guest") {
-        cont.innerHTML = /*html*/`
+  cont = document.getElementById("greetingCont");
+  if ((userName = "Guest")) {
+    cont.innerHTML = /*html*/ `
         <p>Good morning</p>
     `;
-    } else {
-        cont.innerHTML = /*html*/`
+  } else {
+    cont.innerHTML = /*html*/ `
         <p>Good morning,</p><h3>${userName}</h3>
     `;
-    }
+  }
 }
 
 function goToBoard() {
-    window.location.href= 'board.html';
+  window.location.href = "board.html";
 }
 
 function taskMarker() {
-    document.getElementById('summary').classList.add('currentSection');
-}
-
-function hover(element) {
-    todo = document.getElementById("todo");
-    if (element == todo) {
-        element.setAttribute('src', 'Assets/img/edit_white.svg');
-    } else {
-        element.setAttribute('src', 'Assets/img/Done_white.svg');
-    }
-}
-
-function unhover(element) {
-    todo = document.getElementById("todo");
-    if (element == todo) {
-        element.setAttribute('src', 'Assets/img/edit.svg');
-    } else {
-        element.setAttribute('src', 'Assets/img/Done.svg');
-    }
+  document.getElementById("summary").classList.add("currentSection");
 }
