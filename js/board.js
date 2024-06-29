11e08e196b1e61cf52a6cad73045f5d948e24a74
Mr.X <aq-computer@outlook.com>
2024-06-29 21:39:07 +0800

    Added big task setup

--- js/board.js
+++ js/board.js
@@ -385,6 +385,16 @@ function hideAddTaskPopUp() {
   document.getElementById("add-task-pop-up").classList.add("translate-100");
 }
 
+function showBigTaskPopUp() {
+  document.getElementById("show-big-task-pop-up-bg").classList.remove("bg-op-0");
+  document.getElementById("show-big-task-pop-up").classList.remove("translate-100");
+}
+
+function hideBigTaskPopUp() {
+  document.getElementById("show-big-task-pop-up-bg").classList.add("bg-op-0");
+  document.getElementById("show-big-task-pop-up").classList.add("translate-100");
+}
+
 function taskMarker() {
   document.getElementById("board").classList.add("currentSection");
 }
