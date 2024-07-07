eb0ee644bea77c9a18c452c440a15298b32e247f
Mr.X <aq-computer@outlook.com>
2024-07-07 23:42:07 +0800

    merge

--- js/board.js
+++ js/board.js
@@ -312,8 +312,8 @@ function renderBigTask(jsonTextElement) {
 
 // renderSubtask
 function renderSubtask(taskJson) {
-  console.log(taskJson)
-  let renderSubtaskInput = document.getElementById('big-edit-task-subtask-input');
+  console.log(taskJson);
+  let renderSubtaskInput = document.getElementById("big-edit-task-subtask-input");
   if (taskJson.subtask) {
     taskJson.subtask.forEach((subtask) => {
       // console.log(subtask["task-description"]);
@@ -571,6 +571,7 @@ function checkBigEditTaskContact(i, contactObject, taskIndex) {
     </svg>
       `;
   } else {
+    contactObject["isSelected"] = false;
     deleteContactToAssigned(contactObject, taskIndex);
     document.querySelectorAll(".big-edit-task-assigned-to-pop-up-contact-checkbox-icon-container")[i].innerHTML = /*html*/ `
     <svg width="18" height="18" viewBox="0 0 18 18" fill="none" xmlns="http://www.w3.org/2000/svg">
