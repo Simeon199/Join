e9b238295d0dfc56013e9a16b41fc66cd8f02e9d
Simeon199 <sim.ki@outlook.de>
2024-07-16 21:37:13 +0200

    relevant drag and drop functions transfered into new js file (drag and drop mobile)

--- js/board.js
+++ js/board.js
@@ -68,38 +68,6 @@ function hideBoardLoadScreen() {
   document.getElementById("board-add_task-load-screen").classList.add("d-none");
 }
 
-function iterateThroughSubArray(taskArray, htmlElement) {
-  for (i = 0; i < taskArray.length; i++) {
-    let task = taskArray[i];
-    htmlElement.innerHTML += createToDoHTML(task);
-  }
-}
-
-function checkIfEmpty(tasksDiv, divWithoutTasks) {
-  let tasksDivContainer = document.getElementById(tasksDiv);
-  let divWithoutTasksContainer = document.getElementById(divWithoutTasks);
-  if (tasksDivContainer.innerHTML == "") {
-    divWithoutTasksContainer.classList.remove("d-none");
-  }
-}
-
-function updateHTML() {
-  allCategories.forEach((container) => {
-    let element = document.getElementById(container);
-    let oppositeElementName = "no-" + container;
-    let oppositeElement = getRightOppositeElement(oppositeElementName);
-    if (element) {
-      let filteredTasks = tasks.filter((task) => task.container === container);
-      element.innerHTML = "";
-      if (filteredTasks.length > 0) {
-        iterateThroughSubArray(filteredTasks, element);
-      } else {
-        element.innerHTML = oppositeElement;
-      }
-    }
-  });
-}
-
 function setVariableClass(element) {
   let variableClass = "";
   if (element["category"] == "user story") {
@@ -156,26 +124,6 @@ function showTaskContactPlusHTML(lengthOfAssignedTo) {
   `;
 }
 
-function startDragging(elementId) {
-  elementDraggedOver = elementId;
-}
-
-async function moveTo(container) {
-  let oppositeContainer = "no-" + container;
-  let task = tasks.find((task) => task.tasksIdentity == elementDraggedOver);
-  if (task) {
-    task.container = container;
-    saveTasksToLocalStorage();
-    updateHTML();
-    removeEmptyMessage(container, oppositeContainer);
-  }
-  try {
-    await saveTaskToFirebase(task);
-  } catch (error) {
-    console.error("Fehler beim Speichern der tasks in der Firebase-Datenbank:", error);
-  }
-}
-
 async function saveTaskToFirebase(task) {
   const taskPath = `/testRealTasks/${task.tasksIdentity}`;
   const response = await fetch(`${BASE_URL}${taskPath}.json`, {
@@ -190,30 +138,6 @@ async function saveTaskToFirebase(task) {
   } else {
     console.log("Task erfolgreich in Firebase gespeichert");
   }
-}
-
-function removeEmptyMessage(container, oppositeContainer) {
-  let categoryContainer = document.getElementById(container);
-  let oppositeCategoryContainer = document.getElementById(oppositeContainer);
-  if (oppositeCategoryContainer) {
-    categoryContainer.removeChild(oppositeCategoryContainer);
-  }
-}
-
-function getRightOppositeElement(oppositeElementName) {
-  if (oppositeElementName == "no-await-feedback-container") {
-    return returnHtmlNoFeedbackContainer();
-  } else if (oppositeElementName == "no-in-progress-container") {
-    return returnHtmlNoProgressContainer();
-  } else if (oppositeElementName == "no-to-do-container") {
-    return returnHtmlNoToDoContainer();
-  } else if (oppositeElementName == "no-done-container") {
-    return returnHtmlNoDoneContainer();
-  }
-}
-
-function allowDrop(event) {
-  event.preventDefault();
 }
 
 // showAddTaskPopUp
--- js/drag_and_drop_mobile.js
+++ js/drag_and_drop_mobile.js
@@ -1,15 +1,82 @@
-function openMobileDropdown(taskIndex) {
-  //   let mobileDropdown = document.getElementById("mobileDropdown");
-  // if (mobileDropdown.classList.contains("d-none")) {
-  //     mobileDropdown.classList.remove("d-none");
-  // } else {
-  //     mobileDropdown.classList.add("d-none");
-  // }
+function iterateThroughSubArray(taskArray, htmlElement) {
+  for (i = 0; i < taskArray.length; i++) {
+    let task = taskArray[i];
+    htmlElement.innerHTML += createToDoHTML(task);
+  }
+}
 
-  // document.querySelectorAll(".mobileDropdown")[taskIndex].classList.toggle("mobileDropdown-translate-100");
+function checkIfEmpty(tasksDiv, divWithoutTasks) {
+  let tasksDivContainer = document.getElementById(tasksDiv);
+  let divWithoutTasksContainer = document.getElementById(divWithoutTasks);
+  if (tasksDivContainer.innerHTML == "") {
+    divWithoutTasksContainer.classList.remove("d-none");
+  }
+}
+
+function updateHTML() {
+  allCategories.forEach((container) => {
+    let element = document.getElementById(container);
+    let oppositeElementName = "no-" + container;
+    let oppositeElement = getRightOppositeElement(oppositeElementName);
+    if (element) {
+      let filteredTasks = tasks.filter((task) => task.container === container);
+      element.innerHTML = "";
+      if (filteredTasks.length > 0) {
+        iterateThroughSubArray(filteredTasks, element);
+      } else {
+        element.innerHTML = oppositeElement;
+      }
+    }
+  });
+}
+
+function startDragging(elementId) {
+  elementDraggedOver = elementId;
+}
+
+async function moveTo(container) {
+  let oppositeContainer = "no-" + container;
+  let task = tasks.find((task) => task.tasksIdentity == elementDraggedOver);
+  if (task) {
+    task.container = container;
+    saveTasksToLocalStorage();
+    updateHTML();
+    removeEmptyMessage(container, oppositeContainer);
+  }
+  try {
+    await saveTaskToFirebase(task);
+  } catch (error) {
+    console.error("Fehler beim Speichern der tasks in der Firebase-Datenbank:", error);
+  }
+}
+
+function removeEmptyMessage(container, oppositeContainer) {
+  let categoryContainer = document.getElementById(container);
+  let oppositeCategoryContainer = document.getElementById(oppositeContainer);
+  if (oppositeCategoryContainer) {
+    categoryContainer.removeChild(oppositeCategoryContainer);
+  }
+}
+
+function getRightOppositeElement(oppositeElementName) {
+  if (oppositeElementName == "no-await-feedback-container") {
+    return returnHtmlNoFeedbackContainer();
+  } else if (oppositeElementName == "no-in-progress-container") {
+    return returnHtmlNoProgressContainer();
+  } else if (oppositeElementName == "no-to-do-container") {
+    return returnHtmlNoToDoContainer();
+  } else if (oppositeElementName == "no-done-container") {
+    return returnHtmlNoDoneContainer();
+  }
+}
+
+function allowDrop(event) {
+  event.preventDefault();
+}
+
+function openMobileDropdown(taskIndex) {
   let dropdown = document.getElementById(`mobileDropdown${taskIndex}`);
   dropdown.classList.toggle('mobileDropdown-translate-100');
-  // dropdown.classList.toggle('mobileDropdown-translate-0');
 }
 
 async function moveTasksToCategory(taskIndex, newCategory, currentContainer) {
