8553fc08b64798266ae3c0cdd5f2931b14acd6c1
Simeon199 <sim.ki@outlook.de>
2024-07-23 10:10:25 +0200

    script.js is finished

--- script.js
+++ script.js
@@ -54,8 +54,6 @@ function setSessionAttributes() {
  * object to determine if the user is currently logged in as a guest and is not on the "summary.html" page.
  * 
  * @param {Object} obj - An object containing user and login information.
- * @param {string} obj.guestLoginStatus - The guest login status ("true" or other values).
- * @param {string} obj.currentPath - The current path of the window location.
  */
 
 function isGuestLoggedIn(obj) {
@@ -67,10 +65,6 @@ function isGuestLoggedIn(obj) {
  * sessionStorage status and user details.
  * 
  * @param {Object} obj - An object containing login status and user information.
- * @param {string} obj.status - The login status from localStorage ("true" or other values).
- * @param {string} obj.currentUser - The current user email from localStorage.
- * @param {string} obj.sessionStatus - The login status from sessionStorage ("true" or other values).
- * @param {string} obj.sessionUser - The current user email from sessionStorage.
  */
 
 function isUserLoggedIn(obj) {
@@ -83,7 +77,6 @@ function isUserLoggedIn(obj) {
  * It returns `true` if both conditions are met, indicating that a redirect should occur.
  * 
  * @param {Object} obj - An object containing the current path information.
- * @param {string} obj.currentPath - The current path of the window location.
  * @param {boolean} boolean - A flag indicating whether the redirect condition is met.
  */
 
@@ -93,7 +86,7 @@ function shouldRedirect(obj, bolean) {
 }
 
 /**
- * Checks if the user is logged in and manages redirection and session attributes accordingly thereby performing the following actions:
+ * Checks if the user is logged in and manages redirection and session attributes accordingly and thereby performing the following actions:
  * - Redirect the user to the login page if required.
  * - Sets storage attributes if the session status indicates an active session without a current user.
  * - Updates session attributes or removes the "isLoggedIn" item from sessionStorage based on the current path and boolean flag.
@@ -129,6 +122,12 @@ function setStorageAttributes() {
   sessionStorage.removeItem("guestLoginStatus");
 }
 
+/**
+ * Checks if all properties of the given object are null except for `currentPath`.
+ *
+ * @param {Object} obj - The object to be checked.
+ */
+
 function proveIfEverythingIsNullExceptCurrentPath(obj) {
   if (obj["currentPath"] && obj["currentUser"] == null && obj["guestLoginStatus"] == null && obj["sessionUser"] == null) {
     return true;
@@ -136,6 +135,14 @@ function proveIfEverythingIsNullExceptCurrentPath(obj) {
     return false;
   }
 }
+
+/**
+ * Saves the logged-in status of the user in either localStorage or sessionStorage.
+ *
+ * @param {string} name - The nickname of the user.
+ * @param {string} email - The email of the user.
+ * @param {boolean} remember - A flag indicating whether to remember the user across sessions (true) or only for the current session (false).
+ */
 
 function saveLoggedInStatus(name, email, remember) {
   if (remember) {
@@ -150,11 +157,23 @@ function saveLoggedInStatus(name, email, remember) {
   return;
 }
 
+/**
+ * Tests and updates the login status by setting it in sessionStorage if it is not already set in localStorage or sessionStorage.
+ * 
+ */
+
 function testLoginStatus() {
   if (!localStorage.getItem("isLoggedIn") || !sessionStorage.getItem("isLoggedIn")) {
     sessionStorage.setItem("isLoggedIn", "true");
   }
 }
+
+/**
+ * This function posts data to a specified database path using a POST request and throws an error if the network response is not ok or if there is a problem with the registration.
+ *
+ * @param {string} path - The path in the database where the data will be posted.
+ * @param {Object} data - The data to be posted to the database.
+ */
 
 async function postDataToDatabase(path, data) {
   try {
@@ -175,10 +194,21 @@ async function postDataToDatabase(path, data) {
   }
 }
 
+/**
+ * This function displays the registration popup by removing the "d-none" class from its element.
+ * 
+ */
+
 function showRegisterPopup() {
   let registerPopup = document.getElementById("registerPopup");
   registerPopup.classList.remove("d-none");
 }
+
+/**
+ * This function toggles the visibility of the password input field and updates the visibility icons accordingly.
+ *
+ * @param {string} variable - The ID of the password input field to be toggled.
+ */
 
 function showPassword(variable) {
   let passwordContent = document.getElementById(variable);
@@ -189,6 +219,12 @@ function showPassword(variable) {
   checkAllCasesForShowPassword(variable, visibilityInputImage, visibilityInputImageRepeat, visibility, visibilityRepeat);
   checkPasswordContentType(passwordContent);
 }
+
+/**
+ * This function toggles the visibility of the login password input field and updates the visibility and lock icons accordingly.
+ *
+ * @param {string} variable - The ID of the password input field to be toggled.
+ */
 
 function showLoginPassword(variable) {
   let passwordContent = document.getElementById(variable);
@@ -205,6 +241,18 @@ function showLoginPassword(variable) {
   }
   checkPasswordContentType(passwordContent);
 }
+
+/**
+ * This function registers an event listener on an input field to handle visibility changes based on input type and content. Specifically, it concerns the following cases:
+ * - When the input field has content and is of type "password", it shows the `visibilityInputImage` and hides the `visibility` element.
+ * - When the input field has content and is of type "text", it hides the `visibilityInputImage` and shows the `visibility` element.
+ * - When the input field is empty, it shows the `inputLock` element and hides both `visibilityInputImage` and `visibility` elements.
+ *
+ * @param {HTMLElement} inputLock - The HTML element that represents the lock icon or indicator for the input field.
+ * @param {HTMLInputElement} registerInputField - The input field element to which the event listener is attached.
+ * @param {HTMLElement} visibilityInputImage - The HTML element representing the image used for visibility control (e.g., eye icon).
+ * @param {HTMLElement} visibility - The HTML element that shows or hides based on input field type and content.
+ */
 
 function registerInputFieldFunction(inputLock, registerInputField, visibilityInputImage, visibility) {
   registerInputField.addEventListener("input", function () {
@@ -223,6 +271,19 @@ function registerInputFieldFunction(inputLock, registerInputField, visibilityInp
   });
 }
 
+/**
+ * This function registers an event listener on an input field to handle visibility changes based on input type and content. Specifically, it concerns the following cases:
+ * - When the input field has content and is of type "password", it shows the `visibilityInputImageRepeat` element and hides the `visibility` element.
+ * - When the input field has content and is of type "text", it hides the `visibilityInputImageRepeat` element and shows the `visibilityRepeat` element.
+ * - When the input field is empty, it shows the `inputLockRepeat` element and hides both `visibilityInputImageRepeat` and `visibilityRepeat` elements.
+ *
+ * @param {HTMLInputElement} registerInputFieldRepeat - The input field element to which the event listener is attached.
+ * @param {HTMLElement} inputLockRepeat - The HTML element representing the lock icon or indicator for the input field.
+ * @param {HTMLElement} visibilityInputImageRepeat - The HTML element representing the image used for visibility control (e.g., eye icon) specific to the repeat input field.
+ * @param {HTMLElement} visibility - The HTML element that shows or hides based on the input field's type and content for general use.
+ * @param {HTMLElement} visibilityRepeat - The HTML element that shows or hides based on the input field's type and content specifically for the repeat input field.
+ */
+
 function registerInputFieldRepeatFunction(registerInputFieldRepeat, inputLockRepeat, visibilityInputImageRepeat, visibility, visibilityRepeat) {
   registerInputFieldRepeat.addEventListener("input", function () {
     inputLockRepeat.classList.add("d-none");
@@ -239,6 +300,12 @@ function registerInputFieldRepeatFunction(registerInputFieldRepeat, inputLockRep
     }
   });
 }
+
+/**
+ * This function determines the current URL and executes specific orders based on the cases if the page is either 
+ * "login.html" or "register.html". If neither of these pages is detected, it calls `checkIfUserIsLoggedIn`.
+ * 
+ */
 
 function checkWebsiteLocation() {
   let currentURL = window.location.href;
@@ -264,6 +331,13 @@ function checkWebsiteLocation() {
     checkIfUserIsLoggedIn();
   }
 }
+
+/**
+ * The function retrieves several HTML elements by their IDs and organizes them into an object.
+ * The returned object includes references to elements related to visibility controls and input fields
+ * for login and registration purposes.
+ * 
+ */
 
 function createObjectforEventListener() {
   let object = {
