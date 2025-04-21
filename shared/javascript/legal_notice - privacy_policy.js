86eab8c777eb88950a9cafd7635845375f0c51e3
Simeon199 <sim.ki@outlook.de>
2025-04-21 22:17:14 +0200

    sidebar and header now everywhere avaible

--- shared/javascript/legal_notice - privacy_policy.js
+++ shared/javascript/legal_notice - privacy_policy.js
@@ -1,13 +1,21 @@
+import * as shared from '../javascript/shared.js';
+
+document.addEventListener('DOMContentLoaded', () => {
+  shared.bundleLoadingHTMLTemplates();
+})
+
 /**
  * Clears the task marker.
  *
  */
+
 function taskMarker() {}
 
 /**
  * Navigates to the previous page in the browser history.
  *
  */
+
 function previousPage() {
   window.history.back();
 }
